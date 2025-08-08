7c78302448425df670c4a24707f3f29f7872686f
dubeyshubham108 <dubeyshubham154@gmail.com>
2025-08-08 19:43:21 +0530

    connected MongoDB

---
+++ user service/src/index.ts
@@ -0,0 +1,29 @@
+import express from "express"
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const connectDb = async() => {
+    try {
+        mongoose.connect(process.env.MONGO_URI as string, {
+            dbName: "tunewave"
+        });
+        console.log("MongoDB connected");
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Server is working");
+})
+
+const port = process.env.PORT || 5000;
+
+app.listen(5000, () => {
+    console.log(`Server is running on port ${port}`);
+    connectDb()
+})
