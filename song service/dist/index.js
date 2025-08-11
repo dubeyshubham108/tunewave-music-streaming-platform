22682d1db133e3e8d9a954a1a3fa5ad4a7c1e1c3
dubeyshubham108 <dubeyshubham154@gmail.com>
2025-08-11 11:59:18 +0530

    created api for getAllAlbum, getAllSongs, getAllSongsOfAlbum, getSingleSong

--- song service/dist/index.js
+++ song service/dist/index.js
@@ -1,16 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import songRoutes from "./route.js";
-
 dotenv.config();
-
 const app = express();
-
 app.use("/api/v1", songRoutes);
-
-const port  = process.env.PORT || 7000;
-
+const port = process.env.PORT || 7000;
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
 });
-
