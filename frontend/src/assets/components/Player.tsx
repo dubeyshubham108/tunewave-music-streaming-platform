21260de86339ed03c9e4eb978ef5661d266eb7c3
dubeyshubham108 <dubeyshubham154@gmail.com>
2025-08-14 10:54:44 +0530

    created fetchSingleSong, design player component

--- frontend/src/assets/components/Player.tsx
+++ frontend/src/assets/components/Player.tsx
@@ -1,8 +1,30 @@
+import { useEffect } from "react";
+import { useSongData } from "../../context/SongContext";
+
 const Player = () => {
-    return <div>
-        Player
-    </div>
-}
+  const {
+    song,
+    fetchSingleSong,
+    selectedSong,
+    isPlaying,
+    setIsPlaying,
+    prevSong,
+    nextSong,
+  } = useSongData();
+
+  useEffect(() => {
+    fetchSingleSong();
+  }, [selectedSong]);
+  
+  return <div>
+        {
+            song && <div className="h-[10%] bg-black flex justify-between items-center text-white px-4">
+                <div className="lg:flex items-center gap-4">
+                    <img src={song.thumbnail ? song.thumbnail: "./song.jpg"} className="w-12" alt="" />
+                </div>
+                </div>
+        }
+  </div>;
+};
 
 export default Player;
-
