faaf1cc5a141d29f0ef5fb26a31b67af55bc34f0
dubeyshubham108 <dubeyshubham154@gmail.com>
2025-08-14 09:44:00 +0530

    created Album card and mounted it on Home component

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,48 @@
-import Layout from "../assets/components/Layout"
+import Layout from "../assets/components/Layout";
+import { useSongData } from "../context/SongContext";
+import AlbumCard from "../assets/components/AlbumCard";
 
 const Home = () => {
-    return (
-        <div>
-            <Layout>Home</Layout>
+  const { albums, songs } = useSongData();
+  return (
+    <div>
+      <Layout>
+        <div className="mb-4">
+          <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+          <div className="flex overflow-auto">
+            {albums?.map((e, i) => {
+              return (
+                <AlbumCard
+                  key={i}
+                  image={e.thumbnail}
+                  name={e.title}
+                  desc={e.description}
+                  id={e.id}
+                />
+              );
+            })}
+          </div>
         </div>
-    )
-}
 
-export default Home
+        <div className="mb-4">
+          <h1 className="my-5 font-bold text-2xl">Today's biggest hits</h1>
+          <div className="flex overflow-auto">
+            {albums?.map((e, i) => {
+              return (
+                <AlbumCard
+                  key={i}
+                  image={e.thumbnail}
+                  name={e.title}
+                  desc={e.description}
+                  id={e.id}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </Layout>
+    </div>
+  );
+};
+
+export default Home;
