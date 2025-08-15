30173787577a7a0a829719efa547e22d04eb791f
dubeyshubham108 <dubeyshubham154@gmail.com>
2025-08-15 16:07:02 +0530

    added progress bar

--- frontend/src/assets/components/Player.tsx
+++ frontend/src/assets/components/Player.tsx
@@ -12,17 +12,16 @@ const Player = () => {
     nextSong,
   } = useSongData();
 
-  const audioRef = useRef<HTMLAudioElement | null> (null);
+  const audioRef = useRef<HTMLAudioElement | null>(null);
 
-  const [ volume, setVolume ] = useState<Number>(1)
-  const [ progress, setProgress ] = useState<Number>(0);
-  const [ duration, setDuration ] = useState<Number>(0);
-  
+  const [volume, setVolume] = useState<number>(1);
+  const [progress, setProgress] = useState<number>(0);
+  const [duration, setDuration] = useState<number>(0);
+
   useEffect(() => {
-    const audio = audioRef.current
+    const audio = audioRef.current;
 
-    if(!audio)
-      return;
+    if (!audio) return;
 
     const handleLoadedMetaData = () => {
       setDuration(audio.duration || 0);
@@ -42,27 +41,27 @@ const Player = () => {
   }, [song]);
 
   const handlePlayPause = () => {
-    if(audioRef.current) {
-      if(isPlaying) {
-        audioRef.current.pause()
+    if (audioRef.current) {
+      if (isPlaying) {
+        audioRef.current.pause();
       } else {
         audioRef.current.play();
       }
       setIsPlaying(!isPlaying);
     }
-  }
+  };
 
-  const volumeChange= (e: React.ChangeEvent<HTMLInputElement>) => {
+  const volumeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newVolume = parseFloat(e.target.value);
     setVolume(newVolume);
-    if(audioRef.current) {
-      audioRef.current.volume = newVolume
+    if (audioRef.current) {
+      audioRef.current.volume = newVolume;
     }
   };
 
   const durationChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const newTime = (parseFloat(e.target.value)/100) * duration;
-    if(audioRef.current) {
+    const newTime = (parseFloat(e.target.value) / 100) * duration;
+    if (audioRef.current) {
       audioRef.current.currentTime = newTime;
     }
     setProgress(newTime);
@@ -83,14 +82,25 @@ const Player = () => {
               alt=""
             />
             <div className="hidden md:block">
-                <p>{song.title}</p>
-                <p>{song.description?.slice(0, 30)}...</p>
+              <p>{song.title}</p>
+              <p>{song.description?.slice(0, 30)}...</p>
             </div>
           </div>
           <div className="flex flex-col items-center gap-1 m-auto">
             {song.audio && (
-              <audio ref={audioRef} src={song.audio} autoPlay = {isPlaying} />
+              <audio ref={audioRef} src={song.audio} autoPlay={isPlaying} />
             )}
+
+            <div className="w-full items-center flex font-thin text-green-400">
+              <input
+                type="range"
+                min={"0"}
+                max={"100"}
+                className="progress-bar w-[1200px] md:w-[300]"
+                value={(progress/duration) * 100 || 0}
+                onChange={durationChange}
+              />
+            </div>
           </div>
         </div>
       )}
